935787e75739e02dd9f84122bd3f89588eb126bb
Camilo Andres Moreno Roa <48422816+Camilo1423@users.noreply.github.com>
2022-11-08 04:50:41 -0500

    update package

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -5,12 +5,12 @@ import { userRegister, userLogin, userUpdate, userDelete, userGet, userSingleGet
 
 const router = Router()
 
-router.post('register', userRegister)
-router.post('login', userLogin)
-router.put('update/:id', userUpdate)
-router.delete('delete/:id', userDelete)
+router.post('/register', userRegister)
+router.post('/login', userLogin)
+router.put('/update/:id', userUpdate)
+router.delete('/delete/:id', userDelete)
 
-router.get('allusers/:id', userSingleGet)
-router.get('allusers', userGet)
+router.get('/allusers/:id', userSingleGet)
+router.get('/allusers', userGet)
 
 export default router
